fe436b21be08297b73a539fdc13785090371a254
Rodolphe Cambier <rodolphecambier@gmail.com>
2020-04-05 01:57:32 +0200

    no image text box if no image text

--- src/messages/FileMessage.vue
+++ src/messages/FileMessage.vue
@@ -3,10 +3,7 @@
     <div class="sc-message--file-icon">
       <img :src="data.file.url" class="sc-image" />
     </div>
-    <div class="sc-message--file-name" :style="messageColors">
-      <a :href="data.file.url ? data.file.url : '#'" target="_blank">{{data.file.name || ''}}</a>
-    </div>
-    <div class="sc-message--file-text" :style="messageColors">
+    <div class="sc-message--file-text" v-if="data.text" :style="messageColors">
       {{data.text}}
       <p v-if="data.meta" class="sc-message--meta" :style="messageColors">{{data.meta}}</p>
     </div>
